751f5ca50f354950a2af83eb0fbb32a54589888c
conecone <conecone7234@gmail.com>
2024-06-13 23:05:37 +0900

    Progress in creating timer function

--- OnePunchMan/about.xaml.cs
+++ OnePunchMan/about.xaml.cs
@@ -1,5 +1,10 @@
 ﻿namespace OnePunchMan;
 
+using System.Diagnostics;
+using System;
+using System.Timers;
+using System.Diagnostics.Metrics;
+
 public partial class about : ContentPage
 {
     public about()
@@ -7,13 +12,31 @@ public partial class about : ContentPage
         InitializeComponent();
     }
 
-    private void OnTime(object sender, EventArgs e)
+    private void OnClicked_Get_NowTime(object sender, EventArgs e)
     {
         DateTime dt = DateTime.Now;
         TimeText.Text = dt.ToString("yyyy/MM/dd HH:mm:ss");
     }
 
-    private async void OnClicked(object sender, EventArgs e)
+    private void Timer(object sender, EventArgs e)
+    {
+        Timer timer = new Timer();
+        timer.Interval = 1000; // 1秒ごとに処理を行う
+        timer.Elapsed += Timer_Counter;
+        int counter = 0;
+        timer.Start(); // タイマーを開始
+        void Timer_Counter(object sender, ElapsedEventArgs e)
+        {
+            counter++;
+            int h = counter / 3600;
+            int m = (counter - h * 3600) / 60;
+            int s = (counter - h * 3600) - (m * 60);
+            string DisplayTime = String.Format("{0:00}:{1:00}:{2:00}", h, m, s);
+            StopWatch.Text = DisplayTime;
+        }
+    }
+
+    private async void OnClicked_Move_Main(object sender, EventArgs e)
     {
         // サブページへ移動
         await Shell.Current.GoToAsync("//MainPage");
