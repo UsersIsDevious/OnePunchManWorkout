6b08bc6149d6440fa5bed2b2adb36a40cf5b8085
conecone <conecone7234@gmail.com>
2024-06-17 12:06:46 +0900

    Complete Tutorial

---
+++ OnePunchMan/Tutorial.xaml.cs
@@ -0,0 +1,10 @@
+namespace OnePunchMan;
+
+public partial class Tutorial : ContentPage
+{
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
